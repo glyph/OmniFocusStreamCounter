6687800eba8d8bc330395cb2e49bfc309656b170
Glyph <code@glyph.im>
2026-07-15 14:08:27 -0700

    not sure what I'd doing here any more
    This commit was sponsored by Alex Scammon, Brian Grohe, Matt Campbell,
    and my other patrons.  If you want to join them, you can support my
    work at https://glyph.im/patrons/.

--- ofprogress.py
+++ ofprogress.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 from datetime import time, timedelta
-from typing import Any, Callable, Sequence
-import operator
+from typing import Sequence
 
 from appscript import CommandError, app, its, k
 from datetype import DateTime, naive
@@ -12,7 +11,9 @@ from twisted.internet.interfaces import IReactorTime
 from twisted.internet.task import deferLater
 from twisted.logger import Logger, textFileLogObserver
 
+from ofcache import CachedReference
 from oftypes import AppScriptReference, ProgressStatus, SomeTask, SomeTag
+from ofexpr import expression
 
 omnifocus = app("omnifocus")
 log = Logger()
@@ -48,69 +49,6 @@ def available(task: SomeTask) -> bool:
     # """
     # )
     return unblocked and not_deferred and parent_available and tags_available
-
-
-@dataclass
-class PropertyCache:
-    _ref: Any
-    _taskCache: dict[str, SomeTask]
-    _tagCache: dict[str, SomeTag]
-    _properties: dict[object, Any]
-    _cachedTagRefs: list[Any] | None = None
-
-    def __getattr__(self, name: str) -> AppScriptReference[Any]:
-        return CachedReference(lambda: self._properties[getattr(k, name)])
-
-    def parent_task(self) -> Any:
-        # do the same thing with tags?
-        ref = self._properties[k.parent_task]
-        if ref == k.missing_value:
-            return ref
-        # TODO: we already _got_ the .id() but appscript keeps it as private
-        # data, so we have to fish it out like this
-        parentID = ref.AS_aemreference._key
-        if parentID not in self._taskCache:
-            log.info("cache miss for parent ID {tagID}", tagID=parentID)
-            result: Any = PropertyCache(
-                ref, self._taskCache, self._tagCache, ref.properties()
-            )
-            # TODO: if the parent task doesn't match the expression, we store
-            # it in the cache anyway, and that's bad, because it results in a
-            # too-large denominator
-            self._taskCache[parentID] = result
-        return self._taskCache[parentID]
-
-    def tags(self) -> Sequence[SomeTag]:
-        if self._cachedTagRefs is None:
-            # .id() is safe here because it's actually a cached string
-            log.info("cache miss for tags on {id}", id=self.id())
-            self._cachedTagRefs = self._ref.tags()
-        refs = self._cachedTagRefs
-        result = []
-        for ref in refs:
-            # see TODO in parent_task
-            tagID = ref.AS_aemreference._key
-            if tagID not in self._tagCache:
-                log.info("cache miss for tag ID {tagID}", tagID=tagID)
-                # ehhh close enough, parent_task is wrong but attr access
-                # should line up close enough.
-                newCachedTag: Any = PropertyCache(
-                    ref, self._taskCache, self._tagCache, ref.properties()
-                )
-                self._tagCache[tagID] = newCachedTag
-            result.append(self._tagCache[tagID])
-
-        return result
-
-    def properties(self) -> dict[object, Any]:
-        return self._properties
-
-
-def asPropertyCache(
-    taskCache: dict[str, SomeTask], tagCache: dict[str, SomeTag], task: SomeTask
-) -> SomeTask:
-    result: Any = PropertyCache(task, taskCache, tagCache, task.properties())
-    return result
 
 
 @dataclass
